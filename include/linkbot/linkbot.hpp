680118d0bf390893e09c1aff701ea54951a2d598
David Ko <david@barobo.com>
2017-09-13 10:11:58 -0700

    Initial commit

---
+++ include/linkbot/linkbot.hpp
@@ -0,0 +1,12 @@
+
+#ifndef _LINKBOT_HPP_
+#define _LINKBOT_HPP_
+
+#include <string>
+
+class Linkbot {
+    public:
+        std::string test;
+};
+
+#endif
---
+++ src/linkbot.cpp
@@ -0,0 +1,1 @@
+#include <linkbot/linkbot.hpp>
